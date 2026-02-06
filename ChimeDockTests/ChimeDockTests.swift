89de3fb8299af8d0f84922f461d742eee1d56847
Reinier Hernández <sasuke.reinier@gmail.com>
2026-02-06 20:01:56 -0300

    init project

---
+++ ChimeDockTests/ChimeDockTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChimeDockTests.swift
+//  ChimeDockTests
+//
+//  Created by Reinier Hernandez on 06-02-2026.
+//
+
+import Testing
+@testable import ChimeDock
+
+struct ChimeDockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
